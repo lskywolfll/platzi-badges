efafcedda70ed4a8d055ff3179b68aebdc805b7b
lskywolfll <renealejandrosanchezmorales@gmail.com>
2019-11-30 19:14:59 -0300

    Se hacen descripciones y definiciones sobre eÃÃl uso de jsx

---
+++ src/components/Badge.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+class Badge extends React.Component {
+    render() {
+        return (
+            <h1>Badge</h1>
+        )
+    }
+}
+// Exportacion del componente
+export default Badge;
--- src/index.js
+++ src/index.js
@@ -1,7 +1,51 @@
+// const element = document.createElement('h1');
+// element.innerText = 'Hello, Platzi Badges!';
+
+// const container = document.getElementById('app');
+
+// container.appendChild(element);
+// JSX Necesita react y react-dom
 import React from 'react';
 import ReactDOM from 'react-dom';
+// JSX
+// const jsx = <h1>Hello, Platzi Badges from React!</h1>;
+// tipo elemento, atributos(props o propiertes),contenido
+// const element = React.createElement
+//     (
+//         'h1',
+//         // props(funcionalidades o caracteristicas como un objeto)
+//         { href: 'https://platzi.com' },
+//         'hola! Soy los hijos.'
+//     );
+
+// const name = 'Rene';
+
+// // const element = React.createElement('h1', {}, `Hola soy ${name}`);
+
+// const jsx = <h1>Hola soy, {name}</h1>
+
+const element = (
+    <div>
+        <h1>Hola, Soy Rene</h1>
+        <p>Soy Desarrollador Full Stack</p>
+    </div>
+)
 
 const container = document.getElementById('app');
 
-const element = <h1>Hello, Platzi Badges</h1>;
+// Es una forma de crear vistas con javascript de manera mas sencilla, ya que al fin al cabo estamos creando React.createElement por detras al usar JSX por babel y webpack
+// const jsx = <div>
+//     <h1>Hola, soy René</h1>
+//     <p>Soy Desarrollador backend</p>
+// </div>
+
+// const element = React.createElement(
+//     'div',
+//     {},
+//     React.createElement('h1', {}, 'Hola, soy Rene'),
+//     React.createElement('h1', {}, 'Soy desarrollador Full Stack')
+// )
+
+// ReactDOM.render(__qué__,__donde__)
+
 ReactDOM.render(element, container);
